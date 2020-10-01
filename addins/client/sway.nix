d85379ee593153be0d7e2f60770a93b730437e24
Martin Schwaighofer <mschwaig@users.noreply.github.com>
2020-10-02 01:15:21 +0200

    re-add kanshi service (fixed issue in ~/.config)

--- addins/client/sway.nix
+++ addins/client/sway.nix
@@ -69,4 +69,19 @@
   };
 
   programs.waybar.enable = true;
+
+  systemd.user.services.kanshi = {
+    description = "Kanshi output autoconfig ";
+    wantedBy = [ "graphical-session.target" ];
+    partOf = [ "graphical-session.target" ];
+    serviceConfig = {
+      # kanshi doesn't have an option to specifiy config file yet, so it looks
+      # at .config/kanshi/config
+      ExecStart = ''
+      ${pkgs.kanshi}/bin/kanshi
+      '';
+      RestartSec = 5;
+      Restart = "always";
+    };
+  };
 }
