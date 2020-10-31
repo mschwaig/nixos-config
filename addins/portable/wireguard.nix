3d81331e28cd0e5e546af434ae06e7e7dc30fa2f
Martin Schwaighofer <mschwaig@users.noreply.github.com>
2020-10-31 20:51:31 +0100

    change wireguard to newsubnets
    This is not working yet.

--- addins/portable/wireguard.nix
+++ addins/portable/wireguard.nix
@@ -18,11 +18,11 @@
           [WireGuard]
           PrivateKey=${builtins.readFile /home/mschwaig/.wg/privatekey}
           [WireGuardPeer]
-          PublicKey=ECpzlXbRNyuV79RS3qEqp/V9I7qGpKZ4TjXiwiPa4hU=
+          PublicKey=rJxpeBSDQQLtNTIrrxgxbf4yqWKi6acd3N2PEYxWlWE=
           PresharedKey=${builtins.readFile /home/mschwaig/.wg/presharedkey}
           PersistentKeepalive=25
-          AllowedIPs=192.168.9.1, 192.168.1.0/24
-          AllowedIPs=fdf1:e8a1:8d3f:9::1, fdaf:e4ed:daaa::/64
+          AllowedIPs=192.168.98.1, 192.168.97.0/24
+          AllowedIPs=fde4:c86b:cbd3:98::1, fde4:c86b:cbd3:97::1/64
           Endpoint=${builtins.readFile /home/mschwaig/.wg/endpointip}:51820
         '';
       };
@@ -35,26 +35,26 @@
         [Network]
         DHCP=none
         IPv6AcceptRA=false
-        DNS=fdaf:e4ed:daaa::1
-        DNS=192.168.1.1
+        DNS=fde4:c86b:cbd3:97::1
+        DNS=192.168.97.1
         DNSDefaultRoute=true
         Domains=~.
 
 
         # IP addresses the client interface will have
         [Address]
-        Address=192.168.9.2/24
+        Address=192.168.98.2/24
         [Address]
-        Address=fdf1:e8a1:8d3f:9::2/64
+        Address=fde4:c86b:cbd3:98::2/64
         [Address]
         Address=fe80::/64
 
         [Route]
-        Destination=192.168.1.0/24
+        Destination=192.168.97.0/24
         Scope=link
 
         [Route]
-        Destination=fdaf:e4ed:daaa::/64
+        Destination=fde4:c86b:cbd3:97::/60
         Scope=link
       '';
     };
