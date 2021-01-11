4513b09f7d77f227a2f7a52f3d4d96426c82322f
Martin Schwaighofer <mschwaig@users.noreply.github.com>
2021-01-11 21:06:11 +0100

    prevent JKU VPN from autostarting

--- addins/jku-ins-vpn.nix
+++ addins/jku-ins-vpn.nix
@@ -2,6 +2,8 @@
 
 {
   services.openvpn.servers = {
-    jku-ins-vpn  = { config = '' config /root/.openvpn/schwaighofer@ads2-fim.fim.uni-linz.ac.at__ssl_vpn_config.ovpn ''; };
+    jku-ins-vpn  = {
+      autoStart = false;
+      config = '' config /root/.openvpn/schwaighofer@ads2-fim.fim.uni-linz.ac.at__ssl_vpn_config.ovpn '';    };
   };
 }
