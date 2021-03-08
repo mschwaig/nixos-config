6df2d8addedff092bd800e1b76b4307e12b99c22
Martin Schwaighofer <mschwaig@users.noreply.github.com>
2021-03-08 11:57:17 +0100

    combine JKU INS network files

--- addins/jku-ins-network.nix
+++ addins/jku-ins-network.nix
@@ -1,6 +1,23 @@
 { config, pkgs, ... }:
 
 {
+
+  # vpn
+
+  services.openvpn.servers = {
+    jku-ins-vpn  = {
+      autoStart = false;
+      # the line
+      # auth-user-pass
+      # is replaced by
+      # auth-user-pass /root/.openvpn/credentials.txt
+      # inside that file
+      # because systemd trying to prompt for that password was a pain
+      config = '' config /root/.openvpn/schwaighofer@ads2-fim.fim.uni-linz.ac.at__ssl_vpn_config.ovpn '';    };
+  };
+
+  # network share
+
   # See instructions here for samba troubleshooting:
   # https://wiki.samba.org/index.php/LinuxCIFS_troubleshooting
 
