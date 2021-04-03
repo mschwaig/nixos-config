9f6a65b7ccd8931e1220a1426ed4ac284a85c4c0
Martin Schwaighofer <mschwaig@users.noreply.github.com>
2021-04-03 15:19:37 +0200

    update how steam is integrated into the system

--- addins/client/steam.nix
+++ addins/client/steam.nix
@@ -1,10 +1,5 @@
 { config, pkgs, ... }:
 
 {
-  # steam prequesites
-  hardware.opengl.driSupport32Bit = true;
-  hardware.opengl.extraPackages32 = with pkgs.pkgsi686Linux; [ libva ];
-  hardware.pulseaudio.support32Bit = true;
-
-  environment.systemPackages = with pkgs; [ steam ];
+  programs.steam.enable = true;
 }
