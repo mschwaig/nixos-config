ca358198b02cc3ab113136d840bed404df099792
Martin Schwaighofer <mschwaig@users.noreply.github.com>
2021-09-08 13:27:39 +0200

    configure git via home-manager

--- home.nix
+++ home.nix
@@ -1,5 +1,22 @@
 { pkgs, ... }:
 {
+  programs = {
+    git = {
+      enable = true;
+      userName = "Martin Schwaighofer";
+      userEmail = "mschwaig@users.noreply.github.com";
+      extraConfig = {
+        pull = { ff = "only"; };
+        credential = { helper = "cache"; };
+        init = { defaultBranch = "main"; };
+        url = {
+          "ssh://git@git.ins.jku.at/" = {
+            insteadof = "https://git.ins.jku.at/";
+          };
+        };
+      };
+    };
+  };
   wayland.windowManager.sway = {
     enable = true;
     config = {
