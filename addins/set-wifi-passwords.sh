f7dc2a37506426f5ab35a0ad801d1bffe8f146c4
Martin Schwaighofer <mschwaig@users.noreply.github.com>
2021-11-23 14:30:46 +0100

    improve wifi password script - not working yet

--- addins/set-wifi-passwords.sh
+++ addins/set-wifi-passwords.sh
@@ -1,14 +1,13 @@
-# wpa_supplicant="/nix/store/76j9l4d6h5q2gi8qn6mg2x3k069dh5cw-wpa_supplicant-2.9"
 interface="wlp61s0"
 
 getNetworkId() {
-  id=`${wpa_supplicant}/bin/wpa_cli -i $interface list_networks | grep $ssid | awk '{print $1}'`
+  id=`wpa_cli -i $interface list_networks | grep $ssid | awk '{print $1}'`
   echo $id
 }
 
 setSecretForSsid() {
   getNetworkId
-  ${wpa_supplicant}/bin/wpa_cli -i $interface $secretName $id $secret
+  wpa_cli -i $interface $secretName $id $secret
 }
 
 while IFS="," read -r ssid secretName secret
@@ -16,4 +15,4 @@ do
   setSecretForSsid $ssid $secretName $secret
 done < /home/mschwaig/.wifi-passwords.csv
 
-${wpa_supplicant}/bin/wpa_cli -i $interface scan
+wpa_cli -i $interface scan
--- machines/mutalisk.nix
+++ machines/mutalisk.nix
@@ -25,8 +25,12 @@
   networking.interfaces.wlp61s0.useDHCP = true;
 
   powerManagement.resumeCommands = ''
-    ${pkgs.writeShellScriptBin "set-wifi-password" ../addins/set-wifi-passwords.sh}/bin/set-wifi-password
-  '';
+    ${pkgs.writeShellApplication {
+      name = "set-wifi-passwords";
+      runtimeInputs = [ pkgs.wpa_supplicant_ro_ssids pkgs.gawk ];
+      text = ../addins/set-wifi-passwords.sh;
+    }}/bin/set-wifi-passwords
+    '';
 
   users.users.mschwaig = {
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILnU1xQN50B54S98io0kH1xElc9yNqmZMPF0s8QASLaB" ];
