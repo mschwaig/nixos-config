e2e618a6f1ddd3dff40545c20ee460927e0fe7e4
Martin Schwaighofer <mschwaig@users.noreply.github.com>
2022-02-20 10:24:36 +0100

    temporarily switch from nixos-unstable to master
    Since unstable is behind by a week right now and fails to build some python stuff.

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     deploy-rs.url = github:serokell/deploy-rs;
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    nixpkgs.url = github:NixOS/nixpkgs/master;
     nixos-hardware.url = github:NixOS/nixos-hardware/master;
     home-manager = {
       url = github:nix-community/home-manager;
