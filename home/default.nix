42d1f1f21d1779acc5b3c7fed695ff3f1ef81988
Martin Schwaighofer <mschwaig@users.noreply.github.com>
2022-09-03 23:57:51 +0200

    add missing stateVersion for home-manager

--- home/default.nix
+++ home/default.nix
@@ -2,6 +2,7 @@
   graphical = { pkgs, ... }: {
     imports = [ ./text ./graphical ];
     home.file.".config/pyznap/pyznap.conf".source = ./pyznap.conf;
+    home.stateVersion = "22.11";
   };
 
   terminal = { pkgs, ... }: {
