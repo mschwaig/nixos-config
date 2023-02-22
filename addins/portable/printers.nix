9f7f6a550e88e80d6e788d78f39b4975db7cd106
Martin Schwaighofer <mschwaig@users.noreply.github.com>
2023-02-22 15:20:01 +0100

    add support for proprietary HP printers

--- addins/portable/printers.nix
+++ addins/portable/printers.nix
@@ -6,10 +6,11 @@
 
   services.printing = {
     enable = true;
-    drivers = with pkgs; [ gutenprint hplip ];
+    drivers = with pkgs; [ gutenprint hplipWithPlugin ];
   };
   services.avahi.enable = true;
   # Important to resolve .local domains of printers, otherwise you get an error
   # like  "Impossible to connect to XXX.local: Name or service not known"
   services.avahi.nssmdns = true;
+  services.system-config-printer.enable = true;
 }
