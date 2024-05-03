a5c58fc8bea401cea8db100cfa41431679716d81
Martin Schwaighofer <mschwaig@users.noreply.github.com>
2024-05-03 17:11:10 +0200

    fix nixos-version --configuration-revision
    I broke this myself by accident.

--- addins/attest.nix
+++ addins/attest.nix
@@ -1,8 +1,7 @@
 { inputs, lib, ... }: {
   imports = [
-    inputs.nixos-attest.nixosModules.attest
+    #inputs.nixos-attest.nixosModules.attest
   ];
 
-  # let 'nixos-version-json' know about the flake's git revision
   system.configurationRevision = lib.mkIf (inputs.self ? rev) inputs.self.rev;
 }
