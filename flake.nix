07bc1fa678f88558a5379b03ba1c2a91cd967120
Martin Schwaighofer <mschwaig@users.noreply.github.com>
2024-11-26 22:19:42 +0100

    add client devices as deployment targets

--- flake.nix
+++ flake.nix
@@ -67,6 +67,20 @@
     sshOpts = [ "-A" ];
 
     nodes = {
+      hydralisk = {
+        hostname = "hydralisk";
+        profiles.system = {
+          user = "mschwaig";
+          path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.hydralisk;
+        };
+      };
+      mutalisk = {
+        hostname = "mutalisk";
+        profiles.system = {
+          user = "mschwaig";
+          path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.mutalisk;
+        };
+      };
       lair = {
         hostname = "lair";
         profiles.system = {
