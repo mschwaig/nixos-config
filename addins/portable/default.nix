b0fd0ff5e8efc626e5aa7867283835804ee84949
Martin Schwaighofer <mschwaig@users.noreply.github.com>
2024-12-26 15:43:23 +0100

    disable ins cache for now

--- addins/portable/default.nix
+++ addins/portable/default.nix
@@ -15,7 +15,7 @@ in
     ];
     nix = {
       settings = {
-        substituters = [ "http://cache.nix.ins.jku.at" ];
+        # substituters = [ "http://cache.nix.ins.jku.at" ];
         trusted-public-keys = [ "lair.van-duck.ts.net.net:6RWQD3CFGg9OY4bhqPzBumZ+o70lIEVH3R9bxTj+FXw=" "cache.nix.ins.jku.at:CPefhQ5WiLuI6Bc9T8sErWf0n5Jwu5Pl+i3B2tFg+/U=" ];
       };
     };
