0d637d663b300926d76232e81478fbf6607abb06
Martin Schwaighofer <3856390+mschwaig@users.noreply.github.com>
2025-09-23 17:12:18 +0200

    add gemma3 27b

--- addins/llm/models.nix
+++ addins/llm/models.nix
@@ -9,4 +9,14 @@
     url = "https://huggingface.co/unsloth/GLM-4.5-Air-GGUF/resolve/main/Q4_K_M/GLM-4.5-Air-Q4_K_M-00002-of-00002.gguf";
     hash = "sha256-WBJE206BmJQ8C4CcgXMNaOJOCgqN5p5NoQH/pIIFXCc=";
   };
+
+  "gemma-3-27b-it-qat-Q4_0.gguf" = pkgs.fetchurl {
+    url = "https://huggingface.co/ggml-org/gemma-3-27b-it-qat-GGUF/resolve/main/gemma-3-27b-it-qat-Q4_0.gguf";
+    hash = pkgs.lib.fakeHash;
+  };
+
+  "mmproj-model-f16-27B.gguf" = pkgs.fetchurl {
+    url = "https://huggingface.co/ggml-org/gemma-3-27b-it-qat-GGUF/resolve/main/mmproj-model-f16-27B.gguf";
+    hash = pkgs.lib.fakeHash;
+  };
 }
