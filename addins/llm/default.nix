765233cb5ecf0b05babbf68dd541f6db62208ff3
Martin Schwaighofer <3856390+mschwaig@users.noreply.github.com>
2025-09-23 17:54:54 +0200

    add more models

--- addins/llm/default.nix
+++ addins/llm/default.nix
@@ -21,10 +21,25 @@ in
       healthCheckTimeout = 60;
       models = {
         "glm-4.5-air" = {
-          cmd = "${llama-server} --port \${PORT} -m ${modelPath "GLM-4.5-Air-Q4_K_M-00001-of-00002.gguf"} -ngl 20 --no-webui";
+          cmd = "${llama-server} --port \${PORT} -m ${modelPath "GLM-4.5-Air-Q4_K_M-00001-of-00002.gguf"} -ngl 999 --no-webui";
         };
         "gemma-3-27b" = {
-          cmd = "${llama-server} --port \${PORT} -m ${modelPath "gemma-3-27b-it-qat-Q4_0.gguf"} --mmproj ${modelPath "mmproj-model-f16-27B.gguf"} -ngl 20 --no-webui";
+          cmd = "${llama-server} --port \${PORT} -m ${modelPath "gemma-3-27b-it-qat-Q4_0.gguf"} --mmproj ${modelPath "mmproj-model-f16-27B.gguf"} -ngl 999 --no-webui";
+        };
+        "qwen3-30b-a3b" = {
+          cmd = "${llama-server} --port \${PORT} -m ${modelPath "qwen3-30b-a3b-instruct-2507-q8_0.gguf"} -ngl 999 --no-webui";
+        };
+        "gemma-3-12b" = {
+          cmd = "${llama-server} --port \${PORT} -m ${modelPath "gemma-3-12b-it-qat-Q4_0.gguf"} --mmproj ${modelPath "mmproj-model-f16-12B.gguf"} -ngl 999 --no-webui";
+        };
+        "gpt-oss-20b" = {
+          cmd = "${llama-server} --port \${PORT} -m ${modelPath "gpt-oss-20b-mxfp4.gguf"} -ngl 999 --no-webui";
+        };
+        "gpt-oss-120b" = {
+          cmd = "${llama-server} --port \${PORT} -m ${modelPath "gpt-oss-120b-mxfp4-00001-of-00003.gguf"} -ngl 999 --no-webui";
+        };
+        "qwen3-coder-30b-a3b" = {
+          cmd = "${llama-server} --port \${PORT} -m ${modelPath "qwen3-coder-30b-a3b-instruct-q8_0.gguf"} -ngl 999 --no-webui";
         };
       };
     };
--- addins/llm/models.nix
+++ addins/llm/models.nix
@@ -19,4 +19,34 @@
     url = "https://huggingface.co/ggml-org/gemma-3-27b-it-qat-GGUF/resolve/main/mmproj-model-f16-27B.gguf";
     hash = "sha256-+tQ3uBtvYH0sJ0q8d/W2QaptGQ3a/mmPnEF5gdiFNqo=";
   };
+
+  "qwen3-30b-a3b-instruct-2507-q8_0.gguf" = pkgs.fetchurl {
+    url = "https://huggingface.co/ggml-org/Qwen3-30B-A3B-Instruct-2507-Q8_0-GGUF/resolve/main/qwen3-30b-a3b-instruct-2507-q8_0.gguf";
+    hash = pkgs.lib.fakeHash;
+  };
+
+  "gemma-3-12b-it-qat-Q4_0.gguf" = pkgs.fetchurl {
+    url = "https://huggingface.co/ggml-org/gemma-3-12b-it-qat-GGUF/resolve/main/gemma-3-12b-it-qat-Q4_0.gguf";
+    hash = pkgs.lib.fakeHash;
+  };
+
+  "mmproj-model-f16-12B.gguf" = pkgs.fetchurl {
+    url = "https://huggingface.co/ggml-org/gemma-3-12b-it-qat-GGUF/resolve/main/mmproj-model-f16-12B.gguf";
+    hash = pkgs.lib.fakeHash;
+  };
+
+  "gpt-oss-20b-mxfp4.gguf" = pkgs.fetchurl {
+    url = "https://huggingface.co/ggml-org/gpt-oss-20b-GGUF/resolve/main/gpt-oss-20b-mxfp4.gguf";
+    hash = pkgs.lib.fakeHash;
+  };
+
+  "gpt-oss-120b-mxfp4-00001-of-00003.gguf" = pkgs.fetchurl {
+    url = "https://huggingface.co/ggml-org/gpt-oss-120b-GGUF/resolve/main/gpt-oss-120b-mxfp4-00001-of-00003.gguf";
+    hash = pkgs.lib.fakeHash;
+  };
+
+  "qwen3-coder-30b-a3b-instruct-q8_0.gguf" = pkgs.fetchurl {
+    url = "https://huggingface.co/ggml-org/Qwen3-Coder-30B-A3B-Instruct-Q8_0-GGUF/resolve/main/qwen3-coder-30b-a3b-instruct-q8_0.gguf";
+    hash = pkgs.lib.fakeHash;
+  };
 }
