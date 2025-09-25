11b2b8f6dd839eb8f6b273abb40080f66f3b26d5
Martin Schwaighofer <3856390+mschwaig@users.noreply.github.com>
2025-09-25 16:27:26 +0200

    enable templating for gpt-oss models

--- addins/llm/default.nix
+++ addins/llm/default.nix
@@ -33,10 +33,10 @@ in
           cmd = "${llama-server} --port \${PORT} -m ${modelPath "gemma-3-12b-it-qat-Q4_0.gguf"} --mmproj ${modelPath "mmproj-model-f16-12B.gguf"} -ngl 999 --no-webui";
         };
         "gpt-oss-20b" = {
-          cmd = "${llama-server} --port \${PORT} -m ${modelPath "gpt-oss-20b-mxfp4.gguf"} -ngl 999 --no-webui";
+          cmd = "${llama-server} --port \${PORT} -m ${modelPath "gpt-oss-20b-mxfp4.gguf"} --jinja --reasoning-format auto -ngl 999 --no-webui";
         };
         "gpt-oss-120b" = {
-          cmd = "${llama-server} --port \${PORT} -m ${modelPath "gpt-oss-120b-mxfp4-00001-of-00003.gguf"} -ngl 999 --no-webui";
+          cmd = "${llama-server} --port \${PORT} -m ${modelPath "gpt-oss-120b-mxfp4-00001-of-00003.gguf"} --jinja --reasoning-format auto -ngl 999 --no-webui";
         };
         "qwen3-coder-30b-a3b" = {
           cmd = "${llama-server} --port \${PORT} -m ${modelPath "qwen3-coder-30b-a3b-instruct-q8_0.gguf"} -ngl 999 --no-webui";
